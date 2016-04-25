68c88d76d03ed3619e3968c1e50c874d78d38d80
Luka Vandervelden <lukc@upyum.com>
2016-04-25 09:11:45 +0200

    Mise à jour du modèle HTML.

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ FILE="${FILE%.md}"
 # FIXME: We also need to generate an index.
 
 echo "-- Generating ${FILE}.html"
-pandoc --template template.html ${FILE}.md -o $OUTPUT_DIR/${FILE}.html -N
+pandoc --template template.html ${FILE}.md -o $OUTPUT_DIR/${FILE}.html -N --css=https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css --css=pv.css --toc
 
 echo "-- Generating ${FILE}.pdf"
 pandoc ${FILE}.md --latex-engine=xelatex -H header.tex -o $OUTPUT_DIR/${FILE}.pdf -N -V documentclass=article --toc
